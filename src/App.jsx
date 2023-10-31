ad29be391bf98e21323e238d530ed63da59fd6c5
Rockrayhan <khayrulalamdict@gmail.com>
2023-10-31 19:31:13 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Gallery from './Components/Gallery/Gallery';
+
+
+
+function App() {
+  return (
+    <>
+      <Gallery></Gallery>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Gallery/Gallery.jsx
@@ -0,0 +1,21 @@
+import image1 from '../../images/image-1.webp'
+import image2 from '../../images/image-2.webp'
+import image3 from '../../images/image-3.webp'
+import image4 from '../../images/image-4.webp'
+import image5 from '../../images/image-5.webp'
+import image6 from '../../images/image-6.webp'
+import image7 from '../../images/image-7.webp'
+import image8 from '../../images/image-8.webp'
+import image9 from '../../images/image-9.webp'
+import image10 from '../../images/image-10.jpeg'
+import image11 from '../../images/image-11.jpeg'
+
+const Gallery = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Gallery;
